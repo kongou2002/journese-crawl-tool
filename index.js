ced071cf859a2018c1c42991770e1cad29ba0831
kongou <giachan9@gmail.com>
2023-05-27 18:54:09 +0700

    update crawl tool

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const fs = require( 'fs' );
 const SerpApi = require( 'google-search-results-nodejs' );
-const search = new SerpApi.GoogleSearch( process.env.API_KEY );
-
-
+const dotenv = require( 'dotenv' );
+dotenv.config();
+const API_KEY = process.env.API_KEY;
+console.log( API_KEY );
+const search = new SerpApi.GoogleSearch( API_KEY );
 const params = {
     engine: "google_maps",
     type: "search",
     google_domain: "google.com",
-    q: "coffees",
+    q: "coffee",
     hl: "vi",
     ll: "@11.94118562999679,108.45827227925662,14z",
     start: 20
 };
 
-const filePath = 'out.csv';
-const headers = [ 'target', 'title', 'address', 'imgUrl', 'description', 'latitude', 'longitude', 'rating' ];
+const filePath = 'places.csv';
+const headers = [ 'target', 'title', 'address', 'imgUrl', 'description', 'latitude', 'longitude', 'rating', 'place_id' ];
 
 const callback = function ( response ) {
     const rows = [];
 
     response.local_results.forEach( ( item ) => {
-        const { title, address, gps_coordinates, rating, thumbnail, description } = item;
+        const { title, address, gps_coordinates, rating, thumbnail, description, place_id } = item;
         const { latitude, longitude } = gps_coordinates;
 
         // Normalize the data
@@ -32,7 +34,8 @@ const callback = function ( response ) {
             description: normalizeValue( description ),
             latitude: normalizeValue( latitude ),
             longitude: normalizeValue( longitude ),
-            rating: normalizeValue( rating )
+            rating: normalizeValue( rating ),
+            place_id: normalizeValue( place_id )
         };
 
         rows.push( normalizedRow );
@@ -60,16 +63,17 @@ function normalizeValue( value ) {
 
 function appendToCSV( filePath, headers, rows ) {
     const delimiter = ',';
-    const fileExists = fs.existsSync( filePath );
 
     let csvContent = '';
-    if ( !fileExists ) {
+    if ( !fs.existsSync( filePath ) ) {
+        // Create the file with headers
         const headerRow = headers.join( delimiter );
         csvContent += `${ headerRow }\n`;
-    } else {
-        // Read existing content from the file
-        const existingContent = fs.readFileSync( filePath, 'utf8' );
+    }
 
+    // Read existing content from the file
+    if ( fs.existsSync( filePath ) ) {
+        const existingContent = fs.readFileSync( filePath, 'utf8' );
         // Split the existing content into rows
         const existingRows = existingContent.trim().split( '\n' );
 
@@ -79,7 +83,14 @@ function appendToCSV( filePath, headers, rows ) {
                 .filter( ( header ) => header !== 'target' )
                 .map( ( header ) => row[ header ] || '' )
                 .join( delimiter );
-            return !existingRows.includes( rowContent );
+
+            // Check if the place_id already exists in the CSV
+            const existingPlaceIds = existingRows.map( ( row ) => {
+                const columns = row.split( delimiter );
+                return columns[ headers.indexOf( 'place_id' ) ];
+            } );
+
+            return !existingPlaceIds.includes( row.place_id ) && !existingRows.includes( rowContent );
         } );
 
         // If all rows are duplicates, exit the function
@@ -87,7 +98,6 @@ function appendToCSV( filePath, headers, rows ) {
             console.log( 'Data already exists in the CSV file. Skipping...' );
             return;
         }
-
         // Add the unique rows to the CSV content
         uniqueRows.forEach( ( row ) => {
             const values = headers.map( ( header ) => row[ header ] || '' );
@@ -96,8 +106,8 @@ function appendToCSV( filePath, headers, rows ) {
         } );
     }
 
-    // Append the content to the file
-    fs.appendFileSync( filePath, csvContent, { encoding: 'utf8' } );
+    // Append or create the file
+    fs.writeFileSync( filePath, csvContent, { encoding: 'utf8', flag: 'a' } );
 }
 
 search.json( params, callback );
